2f34dffaf821df493314f3102a2ee1dd87b5cf52
Luis García <luisgarcia.ing@gmail.com>
2017-08-02 23:35:48 -0700

    Improved the registry process

--- application/views/home.php
+++ application/views/home.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
 	<title>Registro</title>
 	<link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
+	<script src="<?php echo base_url('js/jquery.min.js'); ?>"></script>
 </head>
 <body>
 	<form class="container" action="<?php echo base_url('Welcome/register'); ?>" method="post">
@@ -33,32 +34,53 @@
 				<input type="text" class="form-control" id="email" name="email" placeholder="Correo Electr&oacute;nico">
 			</div>
 		</div>
-		<div class="form-group row">
-			<h2>Datos de la mascota</h2>
-		</div>
-		<div id="addAnimals">
-			<span>¿Cuántos animales trajiste a la campaña?</span> <input type="text"> <button id="">Registrar animales</button><br><br>
-		</div>
+		<h2>Datos de la mascota</h2>
 		<div>
-			Nombre de la mascota: <br>
-			<input name="animalName[]" type="text" placeholder="Nombre de la Mascota"><br><br>
-			Especie: <br>
-			<select name="species[]">
-				<option value="-1">Escoge una opci&oacute;n</option>
-				<?php foreach($species as $sp) {
-					echo '<option value="' . $sp->id . '">' . $sp->name . '</option>';
-				} ?>	
-			</select><br><br>
-			G&eacute;nero: <br>
-			<select name="gender[]">
-				<option value="-1">Escoge una opci&oacute;n</option>
-				<option value="Macho">Macho</option>
-				<option value="Hembra">Hembra</option>
-			</select><br><br>
-			Es Nervioso: <input type="checkbox" name="nervous[]"><br><br>
-			Es Agresivo: <input type="checkbox" name="aggresive	[]"><br><br>
+			<div class="form-group row">
+				<label for="email" class="col-sm-2 control-label">¿Cuántos animales trajiste a la campaña?:</label>
+				<div class="col-sm-10">
+					<input class="form-control" id="animalQuantity" type="text">
+				</div>
+			</div>
 		</div>
-		<input id="registerSubmit" type="submit" value="Registrar">
+		<div class="form-group row">
+			<div class="col-xs-12 text-center">
+				<a class="btn btn-default" id="addAnimals">Registrar animales</a>
+			</div>
+		</div>
+		<hr/>
+		<div id="pets"></div>
+		
+		<input id="registerSubmit" class="form-control" type="submit" value="Registrar">
+		<br>
+		<br>
+		<br>
 	</form>
 </body>
+<script type="text/javascript">
+	var insertDiv = "<div class=\"form-group row\">";
+	insertDiv += "<label for=\"animalName[]\" class=\"col-sm-2 control-label\">Nombre de la Mascota:</label>";
+	insertDiv += "<div class=\"col-sm-10\"><input name=\"animalName[]\" type=\"text\" class=\"form-control\" placeholder=\"Nombre de la Mascota\"></div></div>";
+	insertDiv += "<div class=\"form-group row\"><label for=\"weight[]\" class=\"col-sm-2 control-label\">Peso</label><div class=\"col-sm-10\">";
+	insertDiv += "<input type=\"text\" class=\"form-control\" name=\"weight[]\" placeholder=\"Peso\"></div></div>"
+	insertDiv += "<div class=\"form-group row\"><label for=\"species[]\" class=\"col-sm-2 control-label\">Especie:</label><div class=\"col-sm-10\">";
+	insertDiv += "<select name=\"species[]\"><option value=\"-1\">Escoge una opci&oacute;n</option>";		
+	insertDiv += "<?php foreach($species as $sp) { echo '<option value=\"' . $sp->id . '\">' . $sp->name . '</option>'; }?>";
+	insertDiv += "</select></div></div><div class=\"form-group row\"><label for=\"gender[]\" class=\"col-sm-2 control-label\">G&eacute;nero:</label>";
+	insertDiv += "<div class=\"col-sm-10\"><select name=\"gender[]\"><option value=\"-1\">Escoge una opci&oacute;n</option>";
+	insertDiv += "<option value=\"Macho\">Macho</option><option value=\"Hembra\">Hembra</option></select></div>";
+	insertDiv += "</div><div class=\"form-group row\"><label for=\"nervous[]\" class=\"col-sm-2 control-label\">¿Nervioso?:</label>";
+	insertDiv += "<div class=\"col-sm-10\"><input type=\"checkbox\" name=\"nervous[]\"></div></div><div class=\"form-group row\">";
+	insertDiv += "<label for=\"aggresive[]\" class=\"col-sm-2 control-label\">¿Agresivo?:</label><div class=\"col-sm-10\"><input type=\"checkbox\" name=\"aggresive[]\"></div></div><hr/>";
+
+	$(function () {
+		$("#addAnimals").click(function(){
+			$('#pets').html("");
+			var animals = parseInt($('#animalQuantity').val());
+			for(var i = 0; i < animals; i++) {
+				$('#pets').append(insertDiv);
+			}
+		});
+	});
+</script>
 </html>
