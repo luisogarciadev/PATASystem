5c9f83351e1c00d733b1c8f0f91d5879e34680fc
Luis Garcia <luisgarcia.ing@gmail.com>
2018-07-28 13:27:46 -0700

    Changes to display surgery view

--- application/controllers/Surgery.php
+++ application/controllers/Surgery.php
@@ -4,7 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Surgery extends CI_Controller {
 
 	public function index() {
+		$this->load->model('AnimalModel');
+		$data['title'] = 'Lista de animales para pesas';
+		$data['animals'] = $this->AnimalModel->select();
 
+		LoadViews('Surgery/List', $data);
 	}
 
 }
