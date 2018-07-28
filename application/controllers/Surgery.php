56c69ac2321c84dabae7d7b266fa133d66bd2651
Luis Garcia <luisgarcia.ing@gmail.com>
2018-07-28 13:59:05 -0700

    Added information from the person to the animal to display

--- application/controllers/Surgery.php
+++ application/controllers/Surgery.php
@@ -7,8 +7,12 @@ class Surgery extends CI_Controller {
 		$this->load->model('AnimalModel');
 		$data['title'] = 'Lista de animales para pesas';
 		$data['animals'] = $this->AnimalModel->select();
+		foreach ($data['animals'] in $a) {
+			$person = $this->AnimalModel->getPersonByAnimalID($a->id);
+			$data['animals']->phone = $person->phone;
+			$data['animals']->personName = $person->personName;
+		}
 
 		LoadViews('Surgery/List', $data);
 	}
-
 }
