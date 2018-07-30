113ce5ba4663ce3f4f317e88eef77ef3590fa74c
Luis Garcia <luisgarcia.ing@gmail.com>
2018-07-30 16:21:14 -0700

    Merge and change to home page

--- application/views/home.php
+++ application/views/home.php
@@ -1,10 +1,10 @@
 <body>
 	<div class="container">
-		<div class="row"><br><a class="btn btn-primary" href="<?php echo base_url('PreRegister');?>">Registro Inicial</a></div>
-		<div class="row"><br><a class="btn btn-primary" href="<?php echo base_url('PreRegister/WeightList');?>">Pesas</a></div>
-		<div class="row"><br><a class="btn btn-primary" href="<?php echo base_url('Register');?>">Registro</a></div>
-		<div class="row"><br><a class="btn btn-primary" href="<?php echo base_url('Surgery');?>">Cirug&iacute;a</a></div>
-		<div class="row"><br><a class="btn btn-primary" href="<?php echo base_url('Recovery/Cats');?>">Recuperaci&oacute;n gatos</a></div>
-		<div class="row"><br><a class="btn btn-primary" href="<?php echo base_url('Recovery/Dogs');?>">Recuperaci&oacute;n perros</a></div>
+		<div class="row"><br><a class="btn btn-primary col-xs-12" href="<?php echo base_url('PreRegister');?>">Registro Inicial</a></div>
+		<div class="row"><br><a class="btn btn-primary col-xs-12" href="<?php echo base_url('PreRegister/WeightList');?>">Pesas</a></div>
+		<div class="row"><br><a class="btn btn-primary col-xs-12" href="<?php echo base_url('Register');?>">Registro</a></div>
+		<div class="row"><br><a class="btn btn-primary col-xs-12" href="<?php echo base_url('Surgery');?>">Cirug&iacute;a</a></div>
+		<div class="row"><br><a class="btn btn-primary col-xs-12" href="<?php echo base_url('Recovery/Cats');?>">Recuperaci&oacute;n gatos</a></div>
+		<div class="row"><br><a class="btn btn-primary col-xs-12" href="<?php echo base_url('Recovery/Dogs');?>">Recuperaci&oacute;n perros</a></div>
 	</div>
 </body>
