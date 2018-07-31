b1498f82c1a06d972f41ac05d17f1b4156215ae9
Luis Garcia <luisgarcia.ing@gmail.com>
2018-07-31 10:52:50 -0700

    Changed design of buttons on home page

--- application/views/home.php
+++ application/views/home.php
@@ -1,10 +1,16 @@
 <body>
 	<div class="container">
-		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-12" href="<?php echo base_url('PreRegister');?>">Registro Inicial</a></div></div>
-		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-12" href="<?php echo base_url('PreRegister/WeightList');?>">Pesas</a></div></div>
-		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-12" href="<?php echo base_url('Register');?>">Registro</a></div></div>
-		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-12" href="<?php echo base_url('Surgery');?>">Cirug&iacute;a</a></div></div>
-		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-12" href="<?php echo base_url('Recovery/Cats');?>">Recuperaci&oacute;n gatos</a></div></div>
-		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-12" href="<?php echo base_url('Recovery/Dogs');?>">Recuperaci&oacute;n perros</a></div></div>
+		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-offset-2 col-xs-8" href="<?php echo base_url('PreRegister');?>">Registro Inicial</a></div></div>
+		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-offset-2 col-xs-8" href="<?php echo base_url('PreRegister/WeightList');?>">Pesas</a></div></div>
+		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-offset-2 col-xs-8" href="<?php echo base_url('Register');?>">Registro</a></div></div>
+		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-offset-2 col-xs-8" href="<?php echo base_url('Surgery');?>">Cirug&iacute;a</a></div></div>
+		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-offset-2 col-xs-8" href="<?php echo base_url('Recovery/Cats');?>">Recuperaci&oacute;n gatos</a></div></div>
+		<div class="row"><br><div class="col-xs-12"><a class="btn btn-primary col-xs-offset-2 col-xs-8" href="<?php echo base_url('Recovery/Dogs');?>">Recuperaci&oacute;n perros</a></div></div>
 	</div>
 </body>
+<style>
+	.btn {
+		padding-top: 30px;
+		padding-bottom: 30px;
+	}
+</style>
